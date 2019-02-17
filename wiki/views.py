f37fc3a9480d6f01133a9c922324f076e168cf94
dheide951 <dheide@uncc.edu>
2019-02-16 23:48:42 -0500

    added registration form

--- wiki/views.py
+++ wiki/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'wiki/base_template.html')
+
+
+def register(request):
+
+    if request.method == "POST":
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('index')
+
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
