0bc7f92f195d3a1db0273e865a1c98b464a96a15
dheide951 <dheide@uncc.edu>
2019-02-24 00:31:44 -0500

    F it

--- agile_wiki/settings.py
+++ agile_wiki/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'agile_wiki.urls'
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "wiki/static")
 #     os.path.join(BASE_DIR, "static")
 # ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
