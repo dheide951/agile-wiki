881a28b349ac4740b43c10b1bb6097d9e6137cd4
dheide951 <hbrown61@uncc.edu>
2019-04-22 14:04:22 -0400

    added articles to admin site

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Article, Discussion, Comment
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Discussion)
+admin.site.register(Comment)
